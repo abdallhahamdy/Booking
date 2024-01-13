10b400c3fdd2f20d87b719d2250e6648dd0f9c30
abdallhahamdy <abdallhahamdy@yahoo.com>
2024-01-13 10:46:41 +0200

    add Entities

---
+++ src/main/java/com/AlTaraf/Booking/entity/Role.java
@@ -0,0 +1,25 @@
+package com.AlTaraf.Booking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    // Constructors, getters, and setters
+}
---
+++ src/main/java/com/AlTaraf/Booking/entity/User.java
@@ -0,0 +1,34 @@
+package com.AlTaraf.Booking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
