31d1c0ac61d0a9e0ef6ea1d479a62f3a7fec764d
abdallhahamdy <abdallhahamdy@yahoo.com>
2024-01-23 20:57:01 +0200

    Auditable

--- src/main/java/com/AlTaraf/Booking/BookingApplication.java
+++ src/main/java/com/AlTaraf/Booking/BookingApplication.java
@@ -1,10 +1,20 @@
 package com.AlTaraf.Booking;
 
+import com.AlTaraf.Booking.config.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class BookingApplication {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new AuditorAwareImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApplication.class, args);
