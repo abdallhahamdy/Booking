1e56136e4c59047ef035039f450501f5df607aa7
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-01-30 16:59:04 +0200

    Edit Auditable

--- src/main/java/com/AlTaraf/Booking/config/AuditorAwareImpl.java
+++ src/main/java/com/AlTaraf/Booking/config/AuditorAwareImpl.java
@@ -7,13 +7,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        if (SecurityContextHolder.getContext().getAuthentication() == null || SecurityContextHolder.getContext().getAuthentication().getName() == null) {
-            return Optional.of("anonymousUser");
-        } else {
-            return Optional.of(SecurityContextHolder.getContext().getAuthentication().getName());
-        }
+ @Override
+ public Optional<String> getCurrentAuditor() {
+  if (SecurityContextHolder.getContext().getAuthentication() == null || SecurityContextHolder.getContext().getAuthentication().getName() == null) {
+   return Optional.of("anonymousUser");
+  } else {
+   return Optional.of(SecurityContextHolder.getContext().getAuthentication().getName());
+  }
 
-    }
+ }
 }
+
+// ----------------- Use below code for spring security -----------------------
+
