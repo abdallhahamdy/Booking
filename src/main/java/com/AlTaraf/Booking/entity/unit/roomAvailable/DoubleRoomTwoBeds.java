84955de58f3c11f3ad563f7ddd2fe9f9a5688d7a
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-02-03 13:19:11 +0200

    test Relations in RoomAvailable

---
+++ src/main/java/com/AlTaraf/Booking/entity/unit/roomAvailable/DoubleRoomTwoBeds.java
@@ -0,0 +1,76 @@
+//package com.AlTaraf.Booking.entity.unit.roomAvailable;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "double_room_two_beds")
+//public class DoubleRoomTwoBeds {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "room_available")
+//    private RoomAvailable roomAvailable;
+//
+//    @Column(name = "NEW_PRICE")
+//    private int newPrice;
+//
+//    @Column(name = "OLD_PRICE")
+//    private int oldPrice;
+//
+//    @Column(name = "NUMBER")
+//    private int number;
+//
+//    public DoubleRoomTwoBeds() {
+//    }
+//
+//    public DoubleRoomTwoBeds(Long id, RoomAvailable roomAvailable, int newPrice, int oldPrice, int number) {
+//        this.id = id;
+//        this.roomAvailable = roomAvailable;
+//        this.newPrice = newPrice;
+//        this.oldPrice = oldPrice;
+//        this.number = number;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public RoomAvailable getRoomAvailable() {
+//        return roomAvailable;
+//    }
+//
+//    public void setRoomAvailable(RoomAvailable roomAvailable) {
+//        this.roomAvailable = roomAvailable;
+//    }
+//
+//    public int getNewPrice() {
+//        return newPrice;
+//    }
+//
+//    public void setNewPrice(int newPrice) {
+//        this.newPrice = newPrice;
+//    }
+//
+//    public int getOldPrice() {
+//        return oldPrice;
+//    }
+//
+//    public void setOldPrice(int oldPrice) {
+//        this.oldPrice = oldPrice;
+//    }
+//
+//    public int getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(int number) {
+//        this.number = number;
+//    }
+//}
