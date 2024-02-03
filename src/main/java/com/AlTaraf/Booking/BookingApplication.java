8f7e56be14671a6c7761d6940a4bb6a36fe76793
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-02-03 22:40:28 +0200

    Authentication Swagger

--- src/main/java/com/AlTaraf/Booking/BookingApplication.java
+++ src/main/java/com/AlTaraf/Booking/BookingApplication.java
@@ -4,11 +4,13 @@ import com.AlTaraf.Booking.config.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAspectJAutoProxy
 public class BookingApplication {
 
 	@Bean
