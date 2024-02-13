842efb102e77d503f4dab48f62bce37893fe943e
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-02-14 00:51:48 +0200

    Role & Login

--- src/main/java/com/AlTaraf/Booking/dto/RoleDto.java
+++ src/main/java/com/AlTaraf/Booking/dto/RoleDto.java
@@ -2,37 +2,17 @@ package com.AlTaraf.Booking.dto;
 
 import com.AlTaraf.Booking.entity.enums.ERole;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoleDto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
     private ERole roleName;
-
-    public RoleDto() {
-    }
-
-    public RoleDto(Long id, ERole roleName) {
-        this.id = id;
-        this.roleName = roleName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public ERole getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(ERole roleName) {
-        this.roleName = roleName;
-    }
 }
--- src/main/java/com/AlTaraf/Booking/payload/request/LoginRequest.java
+++ src/main/java/com/AlTaraf/Booking/payload/request/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.AlTaraf.Booking.payload.request;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigInteger;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank
     private String phone;
@@ -15,36 +19,5 @@ public class LoginRequest {
 
     private boolean stayLoggedIn;
 
-    public LoginRequest() {
-    }
 
-    public LoginRequest(String phone, String password, boolean stayLoggedIn) {
-        this.phone = phone;
-        this.password = password;
-        this.stayLoggedIn = stayLoggedIn;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isStayLoggedIn() {
-        return stayLoggedIn;
-    }
-
-    public void setStayLoggedIn(boolean stayLoggedIn) {
-        this.stayLoggedIn = stayLoggedIn;
-    }
 }
