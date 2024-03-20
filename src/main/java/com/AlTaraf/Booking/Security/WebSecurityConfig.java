74a4a573bfc617c3119f0886a96999233e909c8e
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-03-20 16:46:50 +0200

    Handle Security Remove OAuth Security

--- src/main/java/com/AlTaraf/Booking/Security/WebSecurityConfig.java
+++ src/main/java/com/AlTaraf/Booking/Security/WebSecurityConfig.java
@@ -6,9 +6,6 @@ import com.AlTaraf.Booking.Security.jwt.AuthTokenFilter;
 import com.AlTaraf.Booking.Security.service.UserDetailsServiceImpl;
 import com.AlTaraf.Booking.Service.CustomOAuth2UserService;
 import com.AlTaraf.Booking.Service.user.UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,17 +18,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.core.Authentication;
 
 import java.io.IOException;
 
@@ -86,6 +74,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/users/Register/**").permitAll()
+                                .requestMatchers("http://node6984-env-9709071.fin.libyanspider.cloud").permitAll()
                                 .requestMatchers("/api/units/filter-unit-by-name").permitAll()
                                 .requestMatchers("/api/cities/**").permitAll() // Adjust roles as needed
                                 .requestMatchers("/regions/byCity/**").permitAll() // Adjust roles as needed
@@ -94,42 +83,13 @@ public class WebSecurityConfig {
                                 .requestMatchers("/image/**").permitAll()
                                 .requestMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**", "/bezkoder-documentation/**", "/bezkoder-api-docs/**").permitAll()
                                 .anyRequest().authenticated()
-                ) .oauth2Login(oauth2Login -> oauth2Login
-                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
-                                .oidcUserService(this.oidcUserService())
-                                .userService(this.oauthUserService()) // Ensure you have a bean for OAuth2UserService
-                        )
-                        .successHandler(new AuthenticationSuccessHandler() {
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                                Authentication authentication) throws IOException, ServletException {
-
-                                OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
-                                String email = oauthUser.getAttribute("email");
-                                String name = oauthUser.getAttribute("name");
-                                String phone = oauthUser.getAttribute("phone");
-                                // Assuming userService.processOAuthPostLogin(email) updates user info
-                                userService.processOAuthPostLogin(email, name, phone);
-                                response.sendRedirect("/list");
-                            }
-                        })
                 );
 
         http.authenticationProvider(authenticationProvider());
-
+//
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauthUserService() {
-        // Return your CustomOAuth2UserService instance
-        return customOAuth2UserService;
-    }
-
-    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        // Return an instance of OidcUserService if needed
-        return new OidcUserService();
     }
 
 }
