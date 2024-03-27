25d6b22ea17b9827717edf842ae7d64c8e59e5bb
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-03-27 20:17:56 +0200

    delete notification by user Id

--- src/main/java/com/AlTaraf/Booking/Repository/NotificationRepository.java
+++ src/main/java/com/AlTaraf/Booking/Repository/NotificationRepository.java
@@ -4,11 +4,20 @@ import com.AlTaraf.Booking.Entity.Notifications.Notifications;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notifications, Long> {
 
     Page<Notifications> findByUserId(Long userId, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Notifications n WHERE n.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 
 }
