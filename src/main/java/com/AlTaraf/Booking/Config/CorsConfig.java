3b571536d6c34559e33ef672e827fb92453c2cfd
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-03-29 09:16:36 +0200

    cors config

--- src/main/java/com/AlTaraf/Booking/Config/CorsConfig.java
+++ src/main/java/com/AlTaraf/Booking/Config/CorsConfig.java
@@ -1,17 +1,16 @@
 package com.AlTaraf.Booking.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Locale;
 
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
