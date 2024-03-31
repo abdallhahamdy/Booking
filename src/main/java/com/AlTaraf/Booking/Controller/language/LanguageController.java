878507d2e1257fd8829a9e444711e1d51cd40a70
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-03-31 07:24:54 +0200

    delete localization Temporarily

--- src/main/java/com/AlTaraf/Booking/Controller/language/LanguageController.java
+++ src/main/java/com/AlTaraf/Booking/Controller/language/LanguageController.java
@@ -1,26 +1,32 @@
 package com.AlTaraf.Booking.Controller.language;
 
-import com.AlTaraf.Booking.i18n.I18nUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
-
-@RestController
-@RequestMapping("/Language")
-public class LanguageController {
-
-    @Autowired
-    I18nUtil i18nUtil;
-
-    @GetMapping(value = "/{name}")
-    public String hello(@PathVariable(name = "name") String name) {
-        return i18nUtil.getMessage("hello.message", name);
-
-    }
-
-}
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.LocaleResolver;
+//
+//import java.util.Locale;
+//
+//@RestController
+//@RequestMapping("/Language")
+//public class LanguageController {
+//
+//    @Autowired
+//    private LocaleResolver localeResolver;
+//
+//    @PostMapping("/language/en")
+//    public ResponseEntity<Void> switchToEnglish(HttpServletRequest request, HttpServletResponse response) {
+//        localeResolver.setLocale(request, response, Locale.ENGLISH);
+//        return ResponseEntity.ok().build();
+//    }
+//
+//    @PostMapping("/language/ar")
+//    public ResponseEntity<Void> switchToArabic(HttpServletRequest request, HttpServletResponse response) {
+//        localeResolver.setLocale(request, response, Locale.forLanguageTag("ar"));
+//        return ResponseEntity.ok().build();
+//    }
+//
+//}
