51c2d82ac0d39109d5b23f944e960dcac20c8370
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-04-04 08:41:09 +0200

    Delete Unuseful comments in image Data

--- src/main/java/com/AlTaraf/Booking/Service/image/ImageDataServiceImpl.java
+++ src/main/java/com/AlTaraf/Booking/Service/image/ImageDataServiceImpl.java
@@ -1,6 +1,5 @@
 package com.AlTaraf.Booking.Service.image;
 
-import com.AlTaraf.Booking.Config.ImageConfig;
 import com.AlTaraf.Booking.Entity.Image.ImageData;
 import com.AlTaraf.Booking.Entity.Image.ImageDataForAds;
 import com.AlTaraf.Booking.Entity.Image.ImageProfile;
@@ -12,19 +11,15 @@ import com.AlTaraf.Booking.Repository.image.ImageDataRepository;
 import com.AlTaraf.Booking.Repository.user.UserRepository;
 import io.minio.*;
 import io.minio.errors.MinioException;
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Service
 public class ImageDataServiceImpl implements ImageDataService {
@@ -88,7 +83,8 @@ public class ImageDataServiceImpl implements ImageDataService {
         return new ImageUploadResponse("Image uploaded successfully: " +
                 file.getOriginalFilename());
     }
-//
+
+    //
     public ImageUploadResponse uploadImageProfile(MultipartFile file, Long userId, Boolean image_background) throws IOException {
         byte[] imageDataProfile = file.getBytes();
 
@@ -140,67 +136,5 @@ public class ImageDataServiceImpl implements ImageDataService {
             throw new RuntimeException("Error uploading image to MinIO server", e);
         }
     }
-
-//    private String uploadToMinioServerAds(String filename, String contentType, byte[] data) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-//        try {
-//            MinioClient minioClient = MinioClient.builder()
-//                    .endpoint("https://play.min.io")
-//                    .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-//                    .build();
-//            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket("avatar").build());
-//            if (!found) {
-//                minioClient.makeBucket(MakeBucketArgs.builder().bucket("avatar").build());
-//            } else {
-//                System.out.println("Bucket 'avatar' already exists.");
-//            }
-//            // Upload the data to the MinIO server
-//            minioClient.putObject(PutObjectArgs.builder()
-//                    .bucket("avatar")
-//                    .object(filename)
-//                    .contentType(contentType) // Set the Content-Type
-//                    .stream(new ByteArrayInputStream(data), data.length, -1)
-//                    .build());
-//            return "https://play.min.io/avatar/" + filename; // Return the URL of the uploaded image
-//        } catch (MinioException e) {
-//            throw new RuntimeException("Error uploading image to MinIO server", e);
-//        }
-//    }
-//
-//    private String uploadToMinioServerProfile(String filename, String contentType, byte[] data) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-//        try {
-//            MinioClient minioClient = MinioClient.builder()
-//                    .endpoint("https://play.min.io")
-//                    .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-//                    .build();
-//            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket("avatar").build());
-//            if (!found) {
-//                minioClient.makeBucket(MakeBucketArgs.builder().bucket("avatar").build());
-//            } else {
-//                System.out.println("Bucket 'profile' already exists.");
-//            }
-//            // Upload the data to the MinIO server
-//            minioClient.putObject(PutObjectArgs.builder()
-//                    .bucket("avatar")
-//                    .object(filename)
-//                    .contentType(contentType) // Set the Content-Type
-//                    .stream(new ByteArrayInputStream(data), data.length, -1)
-//                    .build());
-//            return "https://play.min.io/avatar/" + filename; // Return the URL of the uploaded image
-//        } catch (MinioException e) {
-//            throw new RuntimeException("Error uploading image to MinIO server", e);
-//        }
-//    }
 }
-
-
-
-
-
-//private ImageData decompressImageData(ImageData dbImage) {
-//    return ImageData.builder()
-//            .name(dbImage.getName())
-//            .type(dbImage.getType())
-//            .imageData(ImageConfig.decompressImage(dbImage.getImageData()))
-//            .build();
-//}
 
