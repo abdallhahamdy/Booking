804de7d724d75bdc841feede14f3905f0f148853
abdallhahamdy <bbdallhhmdy@gmail.com>
2024-04-06 22:16:35 +0200

    Delete Comments in Favorite Controller

--- src/main/java/com/AlTaraf/Booking/Controller/unit/favorite/favoriteController.java
+++ src/main/java/com/AlTaraf/Booking/Controller/unit/favorite/favoriteController.java
@@ -1,7 +1,6 @@
 package com.AlTaraf.Booking.Controller.unit.favorite;
 
 import com.AlTaraf.Booking.Dto.Unit.UnitDtoFavorite;
-import com.AlTaraf.Booking.Entity.Favorite.UserFavoriteUnit;
 import com.AlTaraf.Booking.Entity.User.User;
 import com.AlTaraf.Booking.Entity.unit.Unit;
 import com.AlTaraf.Booking.Mapper.Unit.UnitFavoriteMapper;
@@ -77,54 +76,15 @@ public class favoriteController {
         return ResponseEntity.ok().body(unitDtoFavoritesPage);
     }
 
-//    @PatchMapping("/{id}/set-favorite")
-//    public ResponseEntity<UnitDtoFavorite> setFavorite(@PathVariable Long id) {
-//        Unit unit = unitService.getUnitById(id);
-//        UserFavoriteUnit favoriteUnit = new UserFavoriteUnit();
-////        favoriteUnit.setUser(this);
-//        favoriteUnit.setUnit(unit);
-////        userFavoriteUnitService.
-//
-//
-//        if (unit != null) {
-//            unit.setFavorite(true); // Set favorite to true
-//            unitService.saveUnit(unit);
-//
-//            // Convert Unit to UnitDtoFavorite using the mapper
-//            UnitDtoFavorite unitDtoFavorite = unitFavoriteMapper.toUnitFavoriteDto(unit);
-//
-//            return new ResponseEntity<>(unitDtoFavorite, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-
-//    @GetMapping("/favorites")
-//    public ResponseEntity<Page<UnitDtoFavorite>> getFavoriteUnitsForUser(
-//            @RequestParam(name = "userId") Long userId,
-//            @RequestParam(defaultValue = "0") int page,
-//            @RequestParam(defaultValue = "4") int size) {
-//
-//        Page<UnitDtoFavorite> favoriteUnits = unitService.getFavoriteUnitsForUser(userId, page, size);
-//
-//        if (!favoriteUnits.isEmpty()) {
-//            return new ResponseEntity<>(favoriteUnits, HttpStatus.OK);
-//        } else {
-////            ApiResponse response = new ApiResponse(204, "No Content");
-////            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
-//            return ResponseEntity.noContent().build();
-//        }
-//    }
-
     @DeleteMapping("/{userId}/{unitId}")
     public ResponseEntity<?> deleteUserFavoriteUnit(@PathVariable Long userId, @PathVariable Long unitId) {
         try {
             userFavoriteUnitService.deleteUserFavoriteUnit(userId, unitId);
             return ResponseEntity.ok().body(new ApiResponse(200, "UserFavoriteUnit deleted successfully."));
         } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(404, "UserFavoriteUnit not found."));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(404, "Not_found.message"));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, "An error occurred while deleting UserFavoriteUnit."));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(500, "An error occurred while deleting UserFavoriteUnit."));
         }
     }
 
