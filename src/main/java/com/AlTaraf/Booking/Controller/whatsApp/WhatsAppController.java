a8cebeb06bb50889e8a1ccac75a10972ae850ed3
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-04-08 16:10:33 +0200

    Whats App

---
+++ src/main/java/com/AlTaraf/Booking/Controller/whatsApp/WhatsAppController.java
@@ -0,0 +1,64 @@
+package com.AlTaraf.Booking.Controller.whatsApp;
+
+import com.AlTaraf.Booking.Entity.Image.Pdf;
+import com.AlTaraf.Booking.Entity.User.User;
+import com.AlTaraf.Booking.Repository.image.PdfRepository;
+import com.AlTaraf.Booking.Repository.user.UserRepository;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/WhatsApp")
+public class WhatsAppController {
+
+    @Autowired
+    PdfRepository pdfRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/sendDocument/{userId}")
+    public String sendDocument(@PathVariable Long userId) throws Exception {
+
+        Pdf pdf = pdfRepository.findByUserIdAndSentIsNull(userId);
+
+        System.out.println("pdf : " + pdf.getId());
+
+        User user = userRepository.findByUserId(userId);
+
+        String message = "Send_Invoice.message";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        System.out.println("ImagePath: " + pdf.getImagePath());
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+        map.add("token", "v8551cd68z16gr2y");
+        map.add("to", user.getPhone());
+        map.add("filename", "Details_Reservation.message");
+        map.add("document", pdf.getImagePath());
+        map.add("caption", "Send_Invoice.message");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        pdf.setSent(Boolean.TRUE);
+        pdfRepository.save(pdf);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.ultramsg.com/instance82647/messages/document";
+        String response = restTemplate.postForObject(url, request, String.class);
+
+        return response;
+    }
+}
