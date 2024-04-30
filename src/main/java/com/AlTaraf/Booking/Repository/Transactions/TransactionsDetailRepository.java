a8a6fd9bc7f5a8c8132fe95afb3d9e36ce9c29a7
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-04-30 17:02:17 +0300

    Delete TransactionsDetail

--- src/main/java/com/AlTaraf/Booking/Repository/Transactions/TransactionsDetailRepository.java
+++ src/main/java/com/AlTaraf/Booking/Repository/Transactions/TransactionsDetailRepository.java
@@ -4,9 +4,11 @@ import com.AlTaraf.Booking.Entity.Transactions.TransactionsDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TransactionsDetailRepository extends JpaRepository<TransactionsDetail, Long> {
@@ -22,5 +24,8 @@ public interface TransactionsDetailRepository extends JpaRepository<Transactions
     @Query("SELECT td FROM TransactionsDetail td WHERE td.transactions.id = :transactionId AND td.phone = :phone")
     Page<TransactionsDetail> findByTransactionsIdAndPhone(@Param("transactionId") Long transactionId, @Param("phone") String phone, Pageable pageable);
 
-
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TransactionsDetail td WHERE td.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 }
