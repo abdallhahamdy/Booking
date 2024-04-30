2fbe6f19d77f0b600de9b6ec34ce13c9911f57ad
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-04-30 17:02:52 +0300

    delete payment

--- src/main/java/com/AlTaraf/Booking/Repository/payment/PayemntRepository.java
+++ src/main/java/com/AlTaraf/Booking/Repository/payment/PayemntRepository.java
@@ -2,9 +2,11 @@ package com.AlTaraf.Booking.Repository.payment;
 
 import com.AlTaraf.Booking.Entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
@@ -12,5 +14,9 @@ public interface PayemntRepository extends JpaRepository<Payment, Long> {
     @Query("SELECT p FROM Payment p WHERE p.custom_ref = :customRef")
     Payment findByCustomRef(@Param("customRef") String customRef);
 
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Payment p WHERE p.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 
 }
