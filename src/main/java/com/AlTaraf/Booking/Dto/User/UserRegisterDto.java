1515bb962e4a4c1c422b6748a0031df7aed63248
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-05-05 03:53:56 +0300

    delete unused UserRegisterDto

--- src/main/java/com/AlTaraf/Booking/Dto/User/UserRegisterDto.java
+++ src/main/java/com/AlTaraf/Booking/Dto/User/UserRegisterDto.java
@@ -1,14 +1,12 @@
 package com.AlTaraf.Booking.Dto.User;
 
 
-import com.AlTaraf.Booking.Dto.Image.ImageDataDTO;
-import com.AlTaraf.Booking.Dto.Image.ImageDataProfileDTO;
+
 import com.AlTaraf.Booking.Dto.cityDtoAndRoleDto.CityDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -21,5 +19,4 @@ public class UserRegisterDto {
     private String password;
     private CityDto city;
     private Set<String> roles;
-//    private List<ImageDataProfileDTO> imageDataProfileDTOS;
 }
