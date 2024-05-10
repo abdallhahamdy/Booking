6854bf0af8d3f78563d29cb49219b2a697a46879
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-05-10 22:18:50 +0300

    Handling FileForProfileRepository for delete user and imageBackgroundFlag

--- src/main/java/com/AlTaraf/Booking/Repository/File/FileForProfileRepository.java
+++ src/main/java/com/AlTaraf/Booking/Repository/File/FileForProfileRepository.java
@@ -1,29 +1,13 @@
 package com.AlTaraf.Booking.Repository.File;
 
 import com.AlTaraf.Booking.Entity.File.FileForProfile;
-import com.AlTaraf.Booking.Entity.Image.ImageData;
-import com.AlTaraf.Booking.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface FileForProfileRepository extends JpaRepository<FileForProfile, String> {
 
-    List<ImageData> findByUserId(Long userId);
 
-    @Modifying
-    @Query("DELETE FROM FileForProfile fp WHERE fp.user.id = :userId AND fp.imageBackgroundFlag = true")
-    void deleteByUserIdAndImageBackgroundTrue(@Param("userId") Long userId);
-
-    @Modifying
-    @Query("DELETE FROM FileForProfile fp WHERE fp.user.id = :userId AND fp.imageBackgroundFlag IS NULL")
-    void deleteByUserIdAndImageBackgroundIsNull(@Param("userId") Long userId);
-
-    void deleteByUser(User user);
 
 }
