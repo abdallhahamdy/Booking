138ddeaaa4bc535c482f06a31c9678761d4db8dc
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-05-29 22:10:59 +0300

    * Whats App Reservation.

--- src/main/java/com/AlTaraf/Booking/Controller/whatsApp/WhatsAppController.java
+++ src/main/java/com/AlTaraf/Booking/Controller/whatsApp/WhatsAppController.java
@@ -1,9 +1,15 @@
 package com.AlTaraf.Booking.Controller.whatsApp;
 
 import com.AlTaraf.Booking.Entity.File.FileForPdf;
+import com.AlTaraf.Booking.Entity.Reservation.Reservations;
 import com.AlTaraf.Booking.Entity.User.User;
 import com.AlTaraf.Booking.Repository.File.FileForPdfRepository;
+import com.AlTaraf.Booking.Repository.Reservation.ReservationRepository;
 import com.AlTaraf.Booking.Repository.user.UserRepository;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
@@ -21,8 +27,11 @@ public class WhatsAppController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ReservationRepository reservationRepository;
+
     @PostMapping("/sendDocument/{userId}")
-    public String sendDocument(@PathVariable Long userId) {
+    public String sendDocument(@PathVariable Long userId, @RequestParam Long reservationId) {
         try {
             FileForPdf pdf = fileForPdfRepository.findByUserIdAndSentFlagIsNull(userId);
 
@@ -45,7 +54,7 @@ public class WhatsAppController {
 
             MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
             map.add("token", "v8551cd68z16gr2y");
-            map.add("to", "02"+user.getPhone());
+            map.add("to", "218"+user.getPhone());
             map.add("filename", "Details_Reservation.message");
             map.add("document", pdf.getFileDownloadUri());
             map.add("caption", "Send_Invoice.message");
@@ -58,6 +67,30 @@ public class WhatsAppController {
             RestTemplate restTemplate = new RestTemplate();
             String url = "https://api.ultramsg.com/instance82647/messages/document";
             String response = restTemplate.postForObject(url, request, String.class);
+
+            Reservations reservations = reservationRepository.findById(reservationId).orElse(null);
+
+            // OkHttpClient setup for sending location
+            OkHttpClient client = new OkHttpClient();
+            okhttp3.RequestBody body = new FormBody.Builder()
+                    .add("token", "v8551cd68z16gr2y")
+//                    .add("to", "0201110495598")
+                    .add("to", "218" + reservations.getUser().getPhone())
+                    .add("address", reservations.getUnit().getCity().getArabicCityName() + " - " +  reservations.getUnit().getRegion().getRegionArabicName())
+                    .add("lat", String.valueOf(reservations.getUnit().getLatForMapping()))
+                    .add("lng", String.valueOf(reservations.getUnit().getLongForMapping()))
+
+                    .build();
+
+            Request locationRequest = new Request.Builder()
+                    .url("https://api.ultramsg.com/instance82647/messages/location")
+                    .post(body)
+                    .addHeader("content-type", "application/x-www-form-urlencoded")
+                    .build();
+
+            Response locationResponse = client.newCall(locationRequest).execute();
+            System.out.println(locationResponse.body().string());
+
 
             return response;
         } catch (Exception e) {
