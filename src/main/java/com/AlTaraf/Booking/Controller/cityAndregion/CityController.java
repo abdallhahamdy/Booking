5cba9c4c62cc8c0a7689a74675660388041a55ec
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-05-31 06:01:01 +0300

    * City Controller Translation.

--- src/main/java/com/AlTaraf/Booking/Controller/cityAndregion/CityController.java
+++ src/main/java/com/AlTaraf/Booking/Controller/cityAndregion/CityController.java
@@ -9,12 +9,15 @@ import com.AlTaraf.Booking.Payload.response.ApiResponse;
 import com.AlTaraf.Booking.Service.cityAndRegion.CityService;
 import com.AlTaraf.Booking.Service.cityAndRegion.RegionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @RestController
@@ -30,51 +33,123 @@ public class CityController {
     @Autowired
     RegionService regionService;
 
+    @Autowired
+    MessageSource messageSource;
+
     @PostMapping("add")
-    public ResponseEntity<?> createCity(@RequestBody CityDto cityDto) {
+    public ResponseEntity<?> createCity(@RequestBody CityDto cityDto,
+                                        @RequestHeader(name = "Accept-Language", required = false) String acceptLanguageHeader) {
         try {
-            CityDto createdCity = cityService.createCity(cityDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(200, "created city is Sucessful "));
+            Locale locale = LocaleContextHolder.getLocale(); // Default to the locale context holder's locale
+
+            if (acceptLanguageHeader != null && !acceptLanguageHeader.isEmpty()) {
+                try {
+                    List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguageHeader);
+                    if (!languageRanges.isEmpty()) {
+                        locale = Locale.forLanguageTag(languageRanges.get(0).getRange());
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Handle the exception if needed
+                    System.out.println("IllegalArgumentException: " + e);
+                }
+            }
+            cityService.createCity(cityDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(200, messageSource.getMessage("city_created_successful.message", null, LocaleContextHolder.getLocale())));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, "created city failed "));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, messageSource.getMessage("internal_server_error.message", null, LocaleContextHolder.getLocale())));
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCity(@PathVariable("id") Long cityId) {
+    public ResponseEntity<?> deleteCity(@PathVariable("id") Long cityId,
+                                        @RequestHeader(name = "Accept-Language", required = false) String acceptLanguageHeader) {
         try {
+            Locale locale = LocaleContextHolder.getLocale(); // Default to the locale context holder's locale
+
+            if (acceptLanguageHeader != null && !acceptLanguageHeader.isEmpty()) {
+                try {
+                    List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguageHeader);
+                    if (!languageRanges.isEmpty()) {
+                        locale = Locale.forLanguageTag(languageRanges.get(0).getRange());
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Handle the exception if needed
+                    System.out.println("IllegalArgumentException: " + e);
+                }
+            }
             cityService.deleteCity(cityId);
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200, messageSource.getMessage("city_deleted_successful.message", null, LocaleContextHolder.getLocale())));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete city");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageSource.getMessage("internal_server_error.message", null, LocaleContextHolder.getLocale()));
         }
     }
 
     @DeleteMapping("/region/{id}")
-    public ResponseEntity<?> deleteRegion(@PathVariable("id") Long regionId) {
+    public ResponseEntity<?> deleteRegion(@PathVariable("id") Long regionId,
+                                          @RequestHeader(name = "Accept-Language", required = false) String acceptLanguageHeader) {
         try {
+            Locale locale = LocaleContextHolder.getLocale(); // Default to the locale context holder's locale
+
+            if (acceptLanguageHeader != null && !acceptLanguageHeader.isEmpty()) {
+                try {
+                    List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguageHeader);
+                    if (!languageRanges.isEmpty()) {
+                        locale = Locale.forLanguageTag(languageRanges.get(0).getRange());
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Handle the exception if needed
+                    System.out.println("IllegalArgumentException: " + e);
+                }
+            }
             regionService.deleteRegion(regionId);
-            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200, "delete region is successful"));
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200, messageSource.getMessage("region_deleted_successful.message", null, LocaleContextHolder.getLocale())));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, "Failed to delete region"));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, messageSource.getMessage("internal_server_error.message", null, LocaleContextHolder.getLocale())));
         }
     }
 
     @PostMapping("{cityId}/add-region")
-    public ResponseEntity<RegionDto> addRegionToCity(@PathVariable("cityId") Long cityId, @RequestBody RegionDto regionDto) {
+    public ResponseEntity<?> addRegionToCity(@PathVariable("cityId") Long cityId, @RequestBody RegionDto regionDto,
+                                             @RequestHeader(name = "Accept-Language", required = false) String acceptLanguageHeader) {
         try {
-            RegionDto addedRegion = regionService.addRegionToCity(cityId, regionDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(addedRegion);
+            Locale locale = LocaleContextHolder.getLocale(); // Default to the locale context holder's locale
+
+            if (acceptLanguageHeader != null && !acceptLanguageHeader.isEmpty()) {
+                try {
+                    List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguageHeader);
+                    if (!languageRanges.isEmpty()) {
+                        locale = Locale.forLanguageTag(languageRanges.get(0).getRange());
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Handle the exception if needed
+                    System.out.println("IllegalArgumentException: " + e);
+                }
+            }
+            regionService.addRegionToCity(cityId, regionDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(200, messageSource.getMessage("region_created_successful.message", null, LocaleContextHolder.getLocale())));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(500, messageSource.getMessage("internal_server_error.message", null, LocaleContextHolder.getLocale())));
         }
     }
 
     @GetMapping("/all")
-    public ResponseEntity<?> getAllCities() {
+    public ResponseEntity<?> getAllCities(@RequestHeader(name = "Accept-Language", required = false) String acceptLanguageHeader) {
+        Locale locale = LocaleContextHolder.getLocale(); // Default to the locale context holder's locale
+
+        if (acceptLanguageHeader != null && !acceptLanguageHeader.isEmpty()) {
+            try {
+                List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguageHeader);
+                if (!languageRanges.isEmpty()) {
+                    locale = Locale.forLanguageTag(languageRanges.get(0).getRange());
+                }
+            } catch (IllegalArgumentException e) {
+                // Handle the exception if needed
+                System.out.println("IllegalArgumentException: " + e);
+            }
+        }
         List<CityDto> cities = cityService.getAllCities();
         if (cities.isEmpty()) {
-            ApiResponse response = new ApiResponse(204, "no_content.message");
+            ApiResponse response = new ApiResponse(204, messageSource.getMessage("no_content.message", null, LocaleContextHolder.getLocale()));
 
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
         } else {
