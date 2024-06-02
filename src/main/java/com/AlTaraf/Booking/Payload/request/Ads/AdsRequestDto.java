a50af2c1ed1cced1bc4dfd8a776720be74affa93
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-02 06:04:55 +0300

    * Editing AdsRequestDto.

--- src/main/java/com/AlTaraf/Booking/Payload/request/Ads/AdsRequestDto.java
+++ src/main/java/com/AlTaraf/Booking/Payload/request/Ads/AdsRequestDto.java
@@ -1,6 +1,5 @@
 package com.AlTaraf.Booking.Payload.request.Ads;
 
-import com.AlTaraf.Booking.Entity.unit.statusUnit.StatusUnit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,16 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdsRequestDto {
-
     private Long unitId;
-//    private List<ImageDataDTO> imageDataDTOs;
-    //    private Long unitTypeId;
     private Long userId;
-//    private Long packageAdsId;
-//    private Long statusUnitId;
-//    private List<ImageData> images;
-//    private String unitName;
-//    private CityDto cityDto;
-//    private RegionDto regionDto;
-//    private Long statusId;
 }
