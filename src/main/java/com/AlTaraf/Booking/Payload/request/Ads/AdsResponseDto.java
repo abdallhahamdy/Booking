b68f6beea129d231df0f5c6c64720e1fe8ba9c36
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-02 06:05:15 +0300

    * Editing AdsResponseDto.

--- src/main/java/com/AlTaraf/Booking/Payload/request/Ads/AdsResponseDto.java
+++ src/main/java/com/AlTaraf/Booking/Payload/request/Ads/AdsResponseDto.java
@@ -1,26 +1,15 @@
 package com.AlTaraf.Booking.Payload.request.Ads;
 
-import com.AlTaraf.Booking.Dto.Image.FileForAdsDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdsResponseDto {
     private Long id;
-//    private ImageDataDTO imageDataDTO;
     private Long unitId;
     private Long userId;
-
-    //    private Long unitTypeId;
-//    private PackageAds packageAds;
-    private List<FileForAdsDTO> fileForAdsDTOS;
-//    private String unitName;
-//    private CityDtoSample cityDto;
-//    private RegionDto regionDto;
-//    private StatusUnit statusUnit;
+    private String imagePath;
 }
